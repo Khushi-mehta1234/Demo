53fcd0ec1a816960aed67f9971aca56b4ffae3d5
Khushi-mehta1234 <khushi.webearl@gmail.com>
2025-09-05 17:28:22 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import StudentData from "./components/StudentData.jsx";
 import ForgotPassword from "./components/ForgotPassword.jsx";
 import LoginForm from "./components/LoginForm.jsx";
-import RegistrationForm from "./components/RegistrationForm.jsx";
+import RegistrationForm from "./components/RegistaerationForm.jsx"; // fixed here
 import UserProfile from "./components/UserProfile.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Country from "./components/Country.jsx";
@@ -20,7 +20,7 @@ function App() {
       <Route path="/ForgotPassword" element={<ForgotPassword />} />
       <Route path="/UserProfile" element={<UserProfile />} />
       <Route path="/StudentData" element={<StudentData />} />
-      <Route path= '/Country' element={<Country/>}/>
+      <Route path="/Country" element={<Country />} />
     </Routes>
   );
 }
