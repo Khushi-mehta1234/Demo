0da54520ff745e43dede5baddd9c2bc0741ef620
Khushi-mehta1234 <khushi.webearl@gmail.com>
2025-09-05 16:55:44 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,26 @@ import React from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import Country from "./components/country";
-import StudentData from "./components/studentData";
-import ForgotPassword from "./components/ForgotPassowrd.jsx";
+
+import StudentData from "./components/StudentData.jsx";
+import ForgotPassword from "./components/ForgotPassword.jsx";
 import LoginForm from "./components/LoginForm.jsx";
-import RegistrationForm from "./components/RegistaerationForm";
-import UserProfile from "./components/UserProfile";
+import RegistrationForm from "./components/RegistrationForm.jsx";
+import UserProfile from "./components/UserProfile.jsx";
 import Navbar from "./components/Navbar.jsx";
+import Country from "./components/country.jsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LoginForm />} />
-
-              <Route path="/user-profile" element={<Navbar />} />
-      <Route path="/registrationForm" element={<RegistrationForm />} />
+      <Route path="/navbar" element={<Navbar />} />
+      <Route path="/registration-form" element={<RegistrationForm />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
       <Route path="/user-profile" element={<UserProfile />} />
-      <Route path="/country" element={<Country/>} />
       <Route path="/student-data" element={<StudentData />} />
+      <Route path='/country' element = {<Country/>}/>
     </Routes>
-   
   );
 }
 
